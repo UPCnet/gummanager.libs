752e455eb521cdabebebde158d39ee3f4d385d6f
Carles Bruguera <carlesba@gmail.com>
2014-02-16 21:49:21 +0100

    Max creation script

--- src/gummanager/libs/_max.py
+++ src/gummanager/libs/_max.py
@@ -1,11 +1,12 @@
 from gummanager.libs.utils import RemoteConnection
 from gummanager.libs.utils import configure_ini
 from gummanager.libs.utils import parse_ini_from
-from gummanager.libs.utils import circus_status
+from gummanager.libs.utils import circus_status, progress_log
 from gummanager.libs.ports import CIRCUS_HTTPD_BASE_PORT
 from gummanager.libs.ports import CIRCUS_TCP_BASE_PORT
-from gummanager.libs.ports import OSIRIS_BASE_PORT
+from gummanager.libs.ports import OSIRIS_BASE_PORT, padded_error, padded_success
 from gummanager.libs.ports import MAX_BASE_PORT
+from gummanager.libs.buildout import RemoteBuildoutHelper
 from gummanager.libs.config_files import LDAP_INI
 from gummanager.libs.config_files import INIT_D_SCRIPT
 from gummanager.libs.config_files import MAX_NGINX_ENTRY
@@ -17,24 +18,25 @@ from collections import OrderedDict
 
 class MaxServer(object):
     _remote_config_files = {}
+
     def __init__(self, *args, **kwargs):
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             setattr(self, k, v)
 
         self.remote = RemoteConnection(self.ssh_user, self.server)
+        self.buildout = RemoteBuildoutHelper(self.remote, self.python_interpreter)
 
     @property
     def remote_config_files(self):
         if not self._remote_config_files:
             code, stdout = self.remote.execute('cd {} && find . -wholename "./*/config/*.ini" | tar cv -O -T -'.format(self.instances_root))
-            tar = tarfile.open(mode= "r:", fileobj = StringIO(stdout))
+            tar = tarfile.open(mode="r:", fileobj=StringIO(stdout))
             for taredfile in tar.members:
                 instance_name, config, filename = taredfile.name.strip('./').split('/')
                 self._remote_config_files.setdefault(instance_name, {})
                 extracted_file = tar.extractfile(taredfile.name)
                 self._remote_config_files[instance_name][filename] = extracted_file.read()
         return self._remote_config_files
-
 
     def get_instances(self):
         instances = []
@@ -49,7 +51,7 @@ class MaxServer(object):
         status = circus_status(
             endpoint=instance['circus_tcp'],
             process='osiris'
-            )
+        )
 
         result_status = OrderedDict()
         result_status['name'] = instance_name
@@ -88,36 +90,52 @@ class MaxServer(object):
         ports.sort()
         return ports[-1] + 1
 
-    def new_instance(self, instance_name):
+    def new_instance(self, instance_name, port_index):
         repo_url = 'https://github.com/UPCnet/maxserver'
         new_instance_folder = '{}/{}'.format(
-            self.instances_root, 
+            self.instances_root,
             instance_name
         )
-        
-        # Clone buildout repository
-        print ' > cloning buildout'
-        code, stdout = self.remote.execute('git clone {} {}'.format(
-            repo_url, 
-            new_instance_folder)
-        )
 
-        if code != 0 or 'Cloning into' not in stdout:
+        self.buildout.folder = new_instance_folder
+        print """
+    Adding a new osiris OAuth server:
+        name: "{}"
+        server: "{}"
+        port_index: "{}"
+
+        """.format(instance_name, self.server, port_index)
+
+        if self.remote.file_exists('{}'.format(new_instance_folder)):
+            padded_error('Folder {} already exists'.format(new_instance_folder))
             return None
 
-        print ' > bootstraping buildout'
-        # Bootstrap instance
-        code, stdout = self.remote.execute('cd {} && {} bootstrap.py -c max-only.cfg'.format(
-            new_instance_folder,
-            self.python_interpreter)
-        )  
+        ###########################################################################################
+        progress_log('Cloning buildout')
 
-        if code != 0 or 'Generated script' not in stdout:
+        success = self.buildout.clone(repo_url)
+
+        if success:
+            padded_success('Succesfully cloned repo at {}'.format(new_instance_folder))
+        else:
+            padded_error('Error when cloning repo')
             return None
 
-        print ' > configuring customizeme.cfg'
-        # Configure customizeme.cfg
-        port_index = '07'
+        ###########################################################################################
+
+        progress_log('Bootstraping buildout')
+
+        success = self.buildout.bootstrap('max-only.cfg')
+
+        if success:
+            padded_success('Succesfully bootstraped buildout {}'.format(new_instance_folder))
+        else:
+            padded_error('Error on bootstraping')
+            return None
+
+        ###########################################################################################
+
+        progress_log('Configuring customizeme.cfg')
 
         customizations = {
             'hosts': {
@@ -133,49 +151,75 @@ class MaxServer(object):
             },
 
         }
-        customizeme = configure_ini(
-            url='https://raw.github.com/UPCnet/maxserver/master/customizeme.cfg',
-            params=customizations)
-        
-        code, stdout = self.remote.execute("cat > {}/customizeme.cfg".format(new_instance_folder),_in=customizeme)
 
-        if code != 0:
+        success = self.buildout.configure_file('customizeme.cfg', customizations)
+
+        if success:
+            padded_success('Succesfully configured {}/customizeme.cfg'.format(new_instance_folder))
+        else:
+            padded_error('Error on applying settings on customizeme.cfg')
             return None
 
-        # # Adding nginx entry
+        ###########################################################################################
 
-        # nginx_params = {
-        #     'instance_name': instance_name,
-        #     'server_dns': self.server_dns,
-        #     'osiris_port': int(port_index) + OSIRIS_BASE_PORT
-        # }
-        # nginxentry = OSIRIS_NGINX_ENTRY.format(**nginx_params)
+        progress_log('Creating nginx entry')
+        nginx_params = {
+            'instance_name': instance_name,
+            'server_dns': self.server_dns,
+            'osiris_port': int(port_index) + OSIRIS_BASE_PORT
+        }
+        nginxentry = MAX_NGINX_ENTRY.format(**nginx_params)
 
-        # code, stdout = self.remote.execute("cat >> {}/config/osiris-instances.ini".format(self.nginx_root),_in=nginxentry)
+        success = self.remote.put_file("{}/config/max-instances/{}.conf".format(self.nginx_root, instance_name), nginxentry)
 
-        print ' > generating init.d script'
-        # Configure startup script
+        if success:
+            padded_success("Succesfully created {}/config/max-instances/{}.conf".format(self.nginx_root, instance_name))
+        else:
+            padded_error('Error when generating nginx config file')
+            return None
+
+        ###########################################################################################
+
+        progress_log('Generating init.d script')
         initd_params = {
             'port_index': port_index,
             'instance_folder': new_instance_folder
         }
         initd_script = INIT_D_SCRIPT.format(**initd_params)
-        code, stdout = self.remote.execute("cat > /etc/init.d/oauth_{}".format(instance_name),_in=initd_script)
+
+        success = self.remote.put_file("/etc/init.d/MAX_{}".format(instance_name), initd_script)
+
+        code, stdout = self.remote.execute("chmod +x /etc/init.d/max_{}".format(instance_name))
         if code != 0:
-            return None
-        code, stdout = self.remote.execute("chmod +x /etc/init.d/oauth_{}".format(instance_name))
-        if code != 0:
-            return None
+            success = False
+
         code, stdout = self.remote.execute("update-rc.d oauth_{} defaults".format(instance_name))
         if code != 0:
+            success = False
+
+        if success:
+            padded_success("Succesfully created /etc/init.d/max_{}".format(instance_name))
+        else:
+            padded_error('Error when generating init.d script')
             return None
 
-        print ' > executing buildout'
-        # Execute buildout
-        code, stdout = self.remote.execute('cd {} && ./bin/buildout -c max-only.cfg'.format(
-            new_instance_folder)
-        )  
+        ###########################################################################################
 
-        if code != 0:
+        progress_log('Executing buildout')
+
+        success = self.buildout.execute()
+        if success:
+            padded_error("Error on buildout execution")
             return None
+        else:
+            padded_success("Succesfully created a new max instance")
+
+
+
+
+
+
+
+
+
 
